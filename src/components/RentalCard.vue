891fc418a6e48339a1757c881a9a20eda7e57548
Dmytro Perepelytsa <umniyd@gmail.com>
2025-07-01 21:31:36 +0300

    feat: implemented truncate fix

--- src/components/RentalCard.vue
+++ src/components/RentalCard.vue
@@ -11,9 +11,9 @@ const props = defineProps<{
 <template>
   <div class="flex justify-between gap-8 border border-black rounded-lg h-64 p-5 pl-10 hover:scale-[1.01] transition-transform duration-500 cursor-pointer">
     <div class="overflow-hidden">
-      <h2 class="text-lg">{{ props.rental.name }}</h2>
+      <h2 class="text-lg truncate max-w-[600px]">{{ props.rental.name }}</h2>
       <p class="text-slate-700 font-medium text-xs">{{ props.rental.location }}</p>
-      <span class="font-medium text-[10px] mb-5 block">{{ props.rental.price }} USD month</span>
+      <span class="font-medium text-[10px] mb-4 block">{{ props.rental.price }} USD month</span>
       <p class="text-sm">{{ props.rental.description }}</p>
     </div>
     <img :src="props.rental.image" :alt="`Image of ${props.rental.name}`" class="hidden md:block w-[216px] h-[216px] object-cover rounded-md">
